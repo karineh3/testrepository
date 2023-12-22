6d9694a953d42b4346a3db5fff089328ce64b2bb
Heinis Karine <coursgretakarine@gmail.com>
2023-12-22 09:55:44 +0100

    messagetest

---
+++ karine.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<Title>Ma première page HTML</title>
+			<meta charset="utf-8">
+			<script>alert ("Bonjour!"); </script>		
+			
+		<title>Tableau HTML</title>
+<style>
+table,th,td{
+border : 1px solid#000;
+border-collapse: collapse;</style>}
+<? php $toto "centaines"
+
+	</head>
+	<body>
+		<h1><span title="Du 09 nov 2023">Formation</span> PHP</h1>
+	<header>Introduction</header
+	<nav> </nav>
+		<div style="color:blue">
+			<p>Nous révisons pour nous mettre à jour de connaissances qui permettra d'entrer dans le vif du sujet.<br>
+			Nous progressons pas à pas et comprenons de mieux en mieux. <samll>le visuel est correct.</small></p>		
+		<article>les outils</article>
+			<p> Il en existe <strong>des centaines avec des tailles appropriées</strong>, des fonctions diverses mais toujours <em>très utiles.</em> Ces outils sont <mark>adaptés</mark> à <b>toutes personnes</b> mais il est important de lire la notice.<p>
+		<section><span>"Récents"</span><span>"Anciens"</span></section>
+		<article>les correspondances</article>		
+			<h1>Tom Sawyer,</h1><time datatime:"2013-11-09">dix ans plutôt</time>
+<hr>
+				<h2>La maisson</h2>
+					<h3> Les meubles</h2>
+						<h4> Le bois utilisé</h4>
+							<h5> Le type de veinure</h5>
+								<h6> La couleur du bois
+<hr>	
+				<h2> Le jardin de 12m<sup>2</sup></h2>
+				<p>Situé <data value="<adress>Maison joyeuse à Antibes<adress> <abbrtitle="morte de rire">lOI</abbr>
+					<H3>Les plantes</h3>
+						<h4>Plantes vertes</h4>
+					<h3>Les fleurs</H3>'<
+			</div>
+<pre>
+     *
+    ***
+   *****
+  *******
+ *********
+ ---------
+ </pre>
+ <code>section</code>
+<!--com-->
+?>
+<bloquequote cite="https://www.dicocitations.com/100-citations-vie.php">Il n'y a qu'un seul bien à la base d'une vie heureuse : la confiance en soi-même.<cite>Beauté</cite></Bloquetote>
+<p>On peut tout se dire mais <cite>beauté</cite></p>
+Une citation-<cite>source</cite>
+<ol type="I">
+	<li>Beurre
+		<ul>
+			<li>mou</li>
+			<li>salé</li>
+		</ul>
+	</li>
+	<li>saucisson</li>
+</ol>
+<ul>
+<li>Pain
+<li>Biscottes
+<ol reversed type="1">
+<li>cachuetes
+<li>pommes
+<Li>concombres
+
+<dl>
+<dt>Arbre</dt>
+	<dd>vert</dd>
+	<dd>mort</dd>
+<dt>Plante</dt>
+	<dd>grimpante</dd>
+	<dd>feuillus</dd>
+
+	
+	<table>
+	<caption>Les chiffres</caption>
+	<colgroup>
+	<col span="3" style="background-color:gold">
+	
+	<col style="background-color:green">
+	</colgroup>
+	<tr><th></th><th>A</th><th>B</th><th>C</th></tr>	
+	<tr><th>1</th><td>A1</td><td>B2</td><td>C3</td></tr>
+	<tr><th>2</th><td>A2</td><td>B3</td><td>C4</td></tr>
+	</table>
+	
+	<table>
+	<tr><th></th><th>A</th><th>B</th><th>C</th></tr>	
+	<tr><th>1</th><td>A1</td><td>B2</td><td>C3</td></tr>
+	<tr>
+	
+	
+	<table>
+	<tr><th></th><th>A</th><th>B</th><th>C</th></tr>	
+	<tr><th>1</th><td>A1</td><td>B2</td><td>C3</td></tr>
+	<tr><th>Somme</th><td></td><td>B3</td></tr>
+	</table>
+	
+<table>
+
+
+<colgroup>
+	<col span="1" style="background-color:blue">
+	
+</Colgroup>
+	<tr><th colspan=3> FINANCES</th></TR>
+	<tr><th colspan=3>Trimestre</th></tr>
+		<th>Janvier</th><th>Fevrier</th><th>Mars</th>
+	<tr><th>12</th><th>14</th><th>16</th>
+
+
+</table>
+
+	</body>
+</html>
